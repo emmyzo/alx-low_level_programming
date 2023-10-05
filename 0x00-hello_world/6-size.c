32f73da170a8cbaebc7f6fcf482fd84c78a5bce3
emmyzo <emmyzofynest@gmail.com>
2023-10-05 14:04:40 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  * Return: 0
  */
 int main(void)
-	{
+{
 		char a ;
 		int b;
 		long int c;
@@ -17,4 +17,4 @@ int main(void)
 		printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 		printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 		return (0);
-	}
+}
