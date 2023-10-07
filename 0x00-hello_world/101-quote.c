89e752e6726f5baf162d296b2225219b0123a6b0
emmyzo <emmyzofynest@gmail.com>
2023-10-07 02:44:39 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main - this prints exactly the input on it
- * Return: always 1.
+ * main - main function
+ * Return: 1
  */
 int main(void)
 	{
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\", 59);
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 		return (1);
 	}
