a232e160d9714f7a42c4c51fd8a7502cbf2712c8
emmyzo <emmyzofynest@gmail.com>
2023-10-07 08:17:57 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - new function
+ * Return: 0
+ */
+
+int main(void)
+{
+	char l;
+
+	for (l = 'z'; l >= 'a'; l--)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
