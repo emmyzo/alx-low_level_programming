4d8c754431e259b3168ed7d5a320c0a2dd007fab
emmyzo <emmyzofynest@gmail.com>
2023-10-16 20:48:40 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - main function
+ * @s: function parameter
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	for (i = 0; i < count / 2; i++)
+	{
+		char n;
+
+		n = s[i];
+			s[i] = s[count - 1 - i];
+			s[count - 1 - i] = n;
+	}
+}
+
