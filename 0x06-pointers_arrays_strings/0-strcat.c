90c05ae41e3c7578ab14d01afbf1c86961183a94
emmyzo <emmyzofynest@gmail.com>
2023-10-19 00:43:35 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,28 @@
  * _strcat - main function
  * @dest: first parameter
  * @src: second parameter
+ * Return: dest
  */
 
 char *_strcat(char *dest, char *src)
 {
+	int a, b;
+
+a = 0;
+b = 0;
+
+while (dest[a] != '\0')
+{
+	a++;
+}
+
+while (src[b] != '\0')
+{
+	dest[b] = src[b];
+	a++;
+	b++;
+}
+dest[a] = '\0';
+return (dest);
+}
+
